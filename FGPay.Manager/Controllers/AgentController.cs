9b235ee04d5de132dc73feb2e09aa4bf04b9ff04
Bruce <Bruce@BRUCE>
2019-09-09 15:59:50 +0800

    代理

---
+++ FGPay.Manager/Controllers/AgentController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FGPay.Manager;
+using FGPay.Models;
+
+namespace FGPay.Manager.Controllers
+{
+    public class AgentController : Controller
+    {
+        private readonly FGPayContext _context;
+
+        public AgentController(FGPayContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Agent
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Agents.ToListAsync());
+        }
+
+        // GET: Agent/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var agent = await _context.Agents
+                .FirstOrDefaultAsync(m => m.AgentID == id);
+            if (agent == null)
+            {
+                return NotFound();
+            }
+
+            return View(agent);
+        }
+
+        // GET: Agent/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Agent/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AgentID,AgentUserID,AgentFullName,Balance,BankFullName,AccountName,AccountCardNumber,Remark,AgentState,Operator,CreateTime,LastUpdateTime,LastLoginTime,ClientIP")] Agent agent)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(agent);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(agent);
+        }
+
+        // GET: Agent/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var agent = await _context.Agents.FindAsync(id);
+            if (agent == null)
+            {
+                return NotFound();
+            }
+            return View(agent);
+        }
+
+        // POST: Agent/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("AgentID,AgentUserID,AgentFullName,Balance,BankFullName,AccountName,AccountCardNumber,Remark,AgentState,Operator,CreateTime,LastUpdateTime,LastLoginTime,ClientIP")] Agent agent)
+        {
+            if (id != agent.AgentID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(agent);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AgentExists(agent.AgentID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(agent);
+        }
+
+        // GET: Agent/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var agent = await _context.Agents
+                .FirstOrDefaultAsync(m => m.AgentID == id);
+            if (agent == null)
+            {
+                return NotFound();
+            }
+
+            return View(agent);
+        }
+
+        // POST: Agent/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var agent = await _context.Agents.FindAsync(id);
+            _context.Agents.Remove(agent);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AgentExists(int id)
+        {
+            return _context.Agents.Any(e => e.AgentID == id);
+        }
+    }
+}

f152223f22fbdfb2a5211e5baec277eae4455658
JY <jason0824@foxmail.com>
2019-09-10 14:43:07 +0800

    添加选单、群组相关MODEL

---
+++ FGPay.Models/Group.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace FGPay.Models
+{
+    /// <summary>
+    /// 群组
+    /// </summary>
+    public class Group
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int GroupID { get; set; }
+
+        [Required]
+        [Display(Name = "群组标识")]
+        [Column(TypeName = "tinyint")]
+        public int GroupTag { get; set; }
+
+        [Display(Name = "群组名称")]
+        [StringLength(20)]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 状态
+        /// </summary>
+        [Required]
+        [Display(Name = "状态")]
+        [Column(TypeName = "tinyint")]
+        public int State { get; set; }
+
+        /// <summary>
+        /// 排序值
+        /// </summary>
+        public int Sort { get; set; }
+    }
+}
---
+++ FGPay.Models/GroupMenu.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace FGPay.Models
+{
+    public class GroupMenu
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        /// <summary>
+        /// 群组ID
+        /// </summary>
+        [Required]
+        [Display(Name = "群组")]
+        public int GroupID { get; set; }
+
+        /// <summary>
+        /// 选单ID
+        /// </summary>
+        [Required]
+        [Display(Name = "选单")]
+        public int MenuID { get; set; }
+
+        [Display(Name = "新增")]
+        public bool Add { get; set; }
+
+        [Display(Name = "修改")]
+        public bool Update { get; set; }
+
+        [Display(Name = "删除")]
+        public bool Delete { get; set; }
+
+        [Display(Name = "其他")]
+        public bool Other { get; set; }
+    }
+}
